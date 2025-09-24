b5e5036dabc1342101d9eccd9327904888ccef97
andewth <dewshelldon@gmail.com>
2025-09-24 13:45:45 +0700

    Dark wall & Dark Orb

---
+++ Assets/ProjectFiles/CoreScripts/Player/k/PlayerHealth.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class PlayerHealth : MonoBehaviour
+{
+    public float maxHP = 100f;
+    public float currentHP;
+
+    public System.Action OnDead;
+
+    void Awake() => currentHP = maxHP;
+
+    public void TakeDamage(float dmgPerSecond)
+    {
+        currentHP -= dmgPerSecond * Time.deltaTime;
+        if (currentHP <= 0)
+        {
+            currentHP = 0;
+            OnDead?.Invoke();
+            
+        }
+    }
+}
+
---
+++ Assets/ProjectFiles/CoreScripts/Player/k/PlayerLight.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class PlayerLight : MonoBehaviour
+{
+    [Header("Link Light2D (Point)")]
+    public Light2D candleLight;
+
+    [Header("Light Radius")]
+    [Tooltip("รัศมีความปลอดภัยจากความมืด")]
+    public float safeRadius = 5f;
+
+    [Header("Flicker")]
+    public bool flicker = true;
+    public float flickerAmp = 0.25f;
+    public float flickerSpeed = 4f;
+
+    public float CurrentRadius { get; private set; }
+
+    void Awake()
+    {
+        if (!candleLight) candleLight = GetComponent<Light2D>();
+        CurrentRadius = safeRadius;
+        ApplyToLight();
+    }
+
+    void Update()
+    {
+        if (flicker)
+        {
+            float a = Mathf.Sin(Time.time * flickerSpeed) * flickerAmp;
+            CurrentRadius = Mathf.Max(0.1f, safeRadius + a);
+            ApplyToLight();
+        }
+    }
+
+    void ApplyToLight()
+    {
+        if (!candleLight) return;
+        candleLight.pointLightOuterRadius = CurrentRadius;
+        candleLight.pointLightInnerRadius = Mathf.Max(0, CurrentRadius - 1.25f);
+    }
+}
